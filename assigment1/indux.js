8d7ac47d59522620f6ed097010a9cd1a1a077318
hafezimran <hafezimran.1992@gmail.com>
2017-10-23 12:39:50 +0500

    alert assigment1

---
+++ assigment1/indux.js
@@ -0,0 +1,7 @@
+// Q # 1
+alert("Error!Please enter a valid password.");
+
+//Q # 2
+alert("welcome to JS land..\n Happy Coding!")
+//Q #3
+alert("Hello..I can run JS through my web browser's console")
